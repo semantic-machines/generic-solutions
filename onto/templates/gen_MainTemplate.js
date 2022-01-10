2621a458c27907c499a4814edb0888254bd39768
Roman Karpov <roman.karpov@gmail.com>
2022-01-10 20:53:50 +0300

    format

--- onto/templates/gen_MainTemplate.js
+++ onto/templates/gen_MainTemplate.js
@@ -1,1 +1,1 @@
-export const html = ` <div about="@" rel="v-s:hasBusinessAspect" data-limit="1" data-template="gen:AspectTemplate"></div> `;
+export const html = '<div about="@" rel="v-s:hasBusinessAspect" data-limit="1" data-template="gen:AspectTemplate"></div>';
